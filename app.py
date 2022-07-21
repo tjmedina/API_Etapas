aebf0b7fecb4df16fdd776153cbe33f20a90294f
tjmedina <quintanatrinos@gmail.com>
2022-07-21 14:39:53 -0400

    API 1

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, abort
+import time
+from model.etapas import MODELO, ETAPAS
+
+app = Flask(__name__)
+
+now = time.strftime("%H:%M:%S")
+
+@app.route('/', methods=['GET'])
+@app.route('/etapas', methods=['GET'])
+def get_all_etapas():
+    return jsonify(Modelo=MODELO, Etapas=ETAPAS, Tempo=now)
+
+@app.route('/etapa/<int:id>', methods=['GET'])
+def una_etapa(id):
+    etapa = ETAPAS.get(id)
+    if not etapa:
+        abort(404)
+    return jsonify(Modelo=MODELO, Etapa=etapa, Tempo=now )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model/etapas.py
@@ -0,0 +1,24 @@
+MODELO = {
+    1:  {
+        'operador': 'Anteros Júnior',
+        'placa': 'XYZ123'
+    }
+}
+
+ETAPAS = {
+    1: {
+        'Descrição': 'Memoria Ram',
+        'Imagem Etapa': '',
+        'Imagem Componente': ''
+    },
+    2: {
+        'Descrição': 'Conetor Bateria',
+        'Imagem Etapa': '',
+        'Imagem Componente': ''
+    },
+    3: {
+        'Descrição': 'Disco Rigido',
+        'Imagem Etapa': '',
+        'Imagem Componente': ''
+    }
+}
