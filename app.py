2c89bee6d04b8134426a331ed3b4ee40f3939295
tjmedina <quintanatrinos@gmail.com>
2022-07-22 11:25:27 -0400

    API 1

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ now = time.strftime("%H:%M:%S")
 @app.route('/', methods=['GET'])
 @app.route('/etapas', methods=['GET'])
 def get_all_etapas():
-    return jsonify(Modelo=MODELO, Etapas=ETAPAS, Tempo=now)
+    return jsonify(Modelo=MODELO, Etapas=ETAPAS, Começar=now)
 
 @app.route('/etapas/<int:id>', methods=['GET'])
 def una_etapa(id):
     etapa = ETAPAS.get(id)
     if not etapa:
         abort(404)
-    return jsonify(Modelo=MODELO, Etapa=etapa, Tempo=now )
+    return jsonify(Modelo=MODELO, Etapa=etapa, Começar=now )
 
 if __name__ == "__main__":
     app.run(debug=True)
